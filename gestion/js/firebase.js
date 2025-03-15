ae725b1f09019d535e65c9f10e69ee70ada3a238
aesgalexis <aesg.alexis@gmail.com>
2025-03-15 16:16:54 +0100

    Update firebase.js

--- gestion/js/firebase.js
+++ gestion/js/firebase.js
@@ -1,28 +1,16 @@
-// Evitar múltiples inicializaciones
-if (!window._firebaseInitialized) {
-    window._firebaseInitialized = true;
+const firebaseConfig = {
+    apiKey: "AIzaSyAst9f2-Jro1d4OX0henL8OeKXT4Ds35uA",
+    authDomain: "factu-f58ab.firebaseapp.com",
+    projectId: "factu-f58ab",
+    storageBucket: "factu-f58ab.firebasestorage.app",
+    messagingSenderId: "126560099435",
+    appId: "1:126560099435:web:483f6c5d68ab3217c3652e",
+    measurementId: "G-MC2S07WMSL"
+};
 
-    // Configuración de Firebase
-    const firebaseConfig = {
-        apiKey: "AIzaSyAst9f2-Jro1d4OX0henL8OeKXT4Ds35uA",
-        authDomain: "factu-f58ab.firebaseapp.com",
-        projectId: "factu-f58ab",
-        storageBucket: "factu-f58ab.firebasestorage.app",
-        messagingSenderId: "126560099435",
-        appId: "1:126560099435:web:483f6c5d68ab3217c3652e",
-        measurementId: "G-MC2S07WMSL"
-    };
-
+// Inicializar Firebase
+if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
-
-    // Definir db y auth como variables globales en `window`
-    window.db = firebase.firestore();
-    window.auth = firebase.auth();
-
-    // Verificar autenticación una sola vez al cargar la página
-    window.auth.onAuthStateChanged(user => {
-        if (!user) {
-            window.location.href = "../"; // Redirigir si no está autenticado
-        }
-    });
 }
+
+const db = firebase.firestore();
