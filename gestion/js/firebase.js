d6e9409106c77c8bf539b4e88becdbf3378f6af7
aesgalexis <aesg.alexis@gmail.com>
2025-03-15 15:03:30 +0100

    Update firebase.js

--- gestion/js/firebase.js
+++ gestion/js/firebase.js
@@ -1,5 +1,7 @@
-// Verificar si Firebase ya está inicializado antes de declararlo de nuevo
-if (!firebase.apps.length) {
+// Evitar que Firebase se inicialice más de una vez
+if (!window.firebaseInitialized) {
+    window.firebaseInitialized = true;
+
     const firebaseConfig = {
         apiKey: "AIzaSyAst9f2-Jro1d4OX0henL8OeKXT4Ds35uA",
         authDomain: "factu-f58ab.firebaseapp.com",
@@ -9,12 +11,11 @@ if (!firebase.apps.length) {
         appId: "1:126560099435:web:483f6c5d68ab3217c3652e",
         measurementId: "G-MC2S07WMSL"
     };
+    
     firebase.initializeApp(firebaseConfig);
+    window.db = firebase.firestore();
+    window.auth = firebase.auth();
 }
-
-// Declarar variables globales de Firebase para Firestore y Auth
-const db = firebase.firestore();
-const auth = firebase.auth();
 
 // Verificar autenticación una sola vez al cargar la página
 auth.onAuthStateChanged(user => {
